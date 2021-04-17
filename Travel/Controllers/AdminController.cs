943dd35cb5f3475f30f5d6dadd52562dddeef6e8
ekinmemis <ekinmemis@outlook.com>
2021-04-17 19:43:00 +0300

    template giydirme

---
+++ Travel/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel.Controllers
+{
+
+
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
